4876fb3b6feb22cac47ee9081d6967d27378faef
Maca-Acha <achamariamacarena@gmail.com>
2023-05-12 10:26:19 -0300

    redux toolkit + css

---
+++ front/src/componentes/Constantes.jsx
@@ -0,0 +1,18 @@
+export const Estudios = [
+    "Primario",
+    "Secundario",
+    "Terciario",
+    "Univercitario",
+    "Otro"]
+export const Generos = [
+    "Femenino",
+    "Masculino",
+    "No-binario",
+    "Otro"]
+        
+export const Tareas = [
+    "Tarea-1",
+    "Tarea-2",
+    "Tarea-3",
+    "Tarea-4",
+    "Otro"]
--- front/src/componentes/Registro.jsx
+++ front/src/componentes/Registro.jsx
@@ -1,42 +1,44 @@
 import { useRef, useState} from "react"
+import {Estudios, Generos, Tareas} from "./Constantes"
 
 function Registro() {
-    const [nombre, setNombre]= useState('')
     const dni = useRef()
-
+    
+    const [nombre, setNombre]= useState('') /* PRUEBA */
     const handleSubmit = (e)=>{
         e.preventDefault() 
         console.log(nombre)   
     }
 
     return(
-        <div>
+        <main>
             <form onSubmit={handleSubmit}>
                 <h1>Registro de personas</h1>
-                <div className="contenedor-nombre">
-                    <label className="label" >
+                <section className="contenedor-nombre">
+                    <label className="label input_grande" >
                         Apellido
                         <input type="text" name="apellido" placeholder="Ej. Rodriguez" />
                     </label>
-                    <label className="label">
+                    <label className="label input_grande">
                         Nombre Completo
                         <input value={nombre} type="text" name="nombre" placeholder="Ej. Juan Ignacio" onChange={(e) => setNombre(e.target.value)}/>
                     </label>
-                </div>
-                <div className="contenedor-dni">
-                    <label className="label">
+                </section>
+                <section className="contenedor-dni">
+                    <label className="label input_mediano">
                         DNI
                         <input type="text" ref={dni} name="dni" placeholder="Ej. 33333333" />
                     </label>
-                    <label className="label" id="cuil">
+                    <label className="label input_mediano">
                         CUIL
                         <input type="text" name="cuil" placeholder="xx-xxxxxxxx-x"/>
                     </label>
-                    <label className="label">
-                        Cargar foto
-                        <input type="file" name="foto" accept="image/*"/>
+                    <label className="label input_mediano" >
+                        Fecha de nacimiento
+                        <input type="date" name="nacimiento" />
                     </label>
-                </div>
+                    
+                </section>
                 <div className="contenedor-domicilio">
                     <label className="label width-domocilio">
                         Domicilio
@@ -44,32 +46,72 @@ function Registro() {
                     </label>
                     <p>mapa</p>
                 </div>
-                <label className="label">
-                    Telefono celular
-                    <input type="text" name="telefono" placeholder="Ej. 115555555"/>
-                </label>
-                <label className="label">
-                    Mail
-                    <input type="text" name="mail" placeholder="Ej. ejemplo@gamil.com"/>
-                </label>
-                <label className="label">
-                    Cargar CV
-                    <input type="file" name="cv" accept=".pdf, .doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document" onChange={e => {console.log(e.target.files)}} />
-                </label>
+                <section className="contenedor-celMail">
+                    <label className="label input_grande">
+                        Telefono celular
+                        <input type="tel" name="telefono" placeholder="Ej. 115555555"/>
+                    </label>
+                    <label className="label input_grande">
+                        Mail
+                        <input type="email" name="mail" placeholder="Ej. ejemplo@gamil.com" />
+                    </label>
+                </section>
+                <section className="cargar_archivo">
+                    <label className="label">
+                        Cargar CV
+                        <input className="cargar" type="file" name="cv" accept=".pdf, .doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document" />
+                    </label>
+                    <label className="label">
+                        Cargar foto
+                        <input className="cargar" type="file" name="foto" accept="image/*"/>
+                    </label>
+                </section>
                 <label className="label">
                     Nivel de Estudios
                     <select>
                         <option disabled="disabled">Seleccione una opcion</option>
-                        <option value="Primaria">Primaria</option>
-                        <option value="Secundario">Secundario</option>
-                        <option value="Universitario">Universitario</option>
-                        <option value="Terciario">Terciario</option>
-                        <option value="Otro">Otro</option>
+                        {Estudios.map((estudio, index)=>{
+                            return <option value={estudio} key={index}>{estudio}</option>
+                        })}
                     </select>
+                </label>
+                <label className="label">
+                    Género 
+                    <div className="generos">
+                        {Generos.map((genero, index)=>{
+                            return <label className="genero" key={index}>
+                                    <input value={genero} type="radio" />
+                                    {genero}
+                                </label>
+                            })}
+                    </div>
+                </label>
+                <section className="militancia">
+                    <label className="label input_mediano">
+                        Tarea
+                        <select>
+                        <option disabled="">Especificar tarea</option>
+                        {Tareas.map((tarea, index)=>{
+                            return <option value={tarea} key={index}>{tarea}</option>
+                        })}
+                    </select>
+                    </label>
+                    <label className="label input_mediano">
+                        Organización
+                        <input type="text" name="orga" placeholder="Especificar organización" />
+                    </label>
+                    <label className="label input_mediano">
+                        Referente
+                        <input type="text" name="referente" placeholder="Especificar referente" />
+                    </label>
+                </section>
+                <label className="label">
+                    Hijos
+                    <input type="text" name="hijos" placeholder="Cantidad de hijos" />
                 </label>
                 <input className="btn-enviar" type="submit" value="Enviar" />
             </form>
-        </div>
+        </main>
     )
 
 }

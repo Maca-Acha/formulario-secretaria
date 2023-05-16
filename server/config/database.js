9158a5af2184275e5f0ff73eeb9cf9b1fe6e562b
Maca-Acha <achamariamacarena@gmail.com>
2023-05-16 13:48:53 -0300

    coneccion base de datos y mongodb

--- server/config/database.js
+++ server/config/database.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose")
 
-const URI = process.env.MONGODB_URI ? process.env.MONGODB_URI : "mongodb://localhost/dbtest"
+const URI = process.env.MONGODB_URI 
 
-mongoose.connect(URI)
-
-const connection = mongoose.connection
-
-connection.once("open", () => {
-    console.log('la base de datos se ha conectado', URI)
+mongoose.connect(URI, {
+    useUnifiedTopology : true,
+    useNewUrlParser: true,
 })
+
+.then (()=> console.log('Database connected'))
+.catch(err => console.error(err))
